113626d8495f66b8abb589556ea662c0a0096d90
Stephan Nielsen <stephannielsen@gmail.com>
2017-04-04 21:36:23 +0200

    Added first layout ideas, configured dev environment with lite-server.
    Added package.json and automatic start of lite-server and chrome/firefox dev environment on npm start.
    Tried metaweather.com as weather api but doesn't support CORS which prevents using it via javascript.
    Updated gitignore for node.

--- local_weather/js/main.js
+++ local_weather/js/main.js
@@ -1,5 +1,24 @@
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(handlePosition);
+    }
+}
 
+function handlePosition(position) {
+  var lat = position.coords.latitude;
+  var lon = position.coords.longitude;
+  
+  $.getJSON("https://www.metaweather.com/api/location/search/?lattlong=" + lat + "," + lon, (json) => {
+    var woeid = json.woeid;
+    $.getJSON("https://www.metaweather.com/api/location/" + woeid, (json) => {
+      var weather = json.consolidated_weather;
+      var name = json.title;
+      var temp = weather.the_temp;
+      $('#weather').text(temp + '°C, ' + name);
+    });
+  });
+}
 
 $(document).ready(function () {
-  //do stuff
+  getLocation();
 });
