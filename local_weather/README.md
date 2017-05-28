7c278b620ecebaa70fadcc47db9683c43b0d071f
Unknown <stephannielsen@gmail.com>
2017-05-28 23:23:41 +0200

    Implemented OpenWeatherMap as weather data source.

--- local_weather/README.md
+++ local_weather/README.md
@@ -1,7 +1,8 @@
 # Show Local Weather - FreeCodeCamp Challenge
 
-To debug this webpage:
+To run this webpage:
 
 - Install NodeJS
 - Run `npm install` in this folder
+- Rename `empty-config.js` to `config.js` and insert your OpenWeatherMap Api key
 - Run `npm start` opens index.html in FF/Chrome
--- local_weather/js/main.js
+++ local_weather/js/main.js
@@ -8,14 +8,20 @@ function handlePosition(position) {
   var lat = position.coords.latitude;
   var lon = position.coords.longitude;
   
-  $.getJSON("https://www.metaweather.com/api/location/search/?lattlong=" + lat + "," + lon, (json) => {
-    var woeid = json.woeid;
-    $.getJSON("https://www.metaweather.com/api/location/" + woeid, (json) => {
-      var weather = json.consolidated_weather;
-      var name = json.title;
-      var temp = weather.the_temp;
-      $('#weather').text(temp + '°C, ' + name);
-    });
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + openWeatherMapApiKey, (json) => {
+    var result = json;
+
+//     {"coord":{"lon":139,"lat":35},
+// "sys":{"country":"JP","sunrise":1369769524,"sunset":1369821049},
+// "weather":[{"id":804,"main":"clouds","description":"overcast clouds","icon":"04n"}],
+// "main":{"temp":289.5,"humidity":89,"pressure":1013,"temp_min":287.04,"temp_max":292.04},
+// "wind":{"speed":7.31,"deg":187.002},
+// "rain":{"3h":0},
+// "clouds":{"all":92},
+// "dt":1369824698,
+// "id":1851632,
+// "name":"Shuzenji",
+// "cod":200}
   });
 }
 
