df374536a48d0cb251323a804ff21faff74721ae
Stephan Nielsen <stephannielsen@gmail.com>
2018-01-05 22:28:09 +0100

    Fetch data from wikipedia and build html.

--- wiki_viewer/README.md
+++ wiki_viewer/README.md
@@ -5,5 +5,3 @@ To run this webpage:
 - Install NodeJS
 - Run `npm install` in this folder
 - Run `npm start` opens index.html in FF/Chrome
-
-https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages|extracts&generator=search&pithumbsize=200&exsentences=1&gsrsearch=flensburg&gsrnamespace=0&gsrinfo=totalhits|suggestion|rewrittenquery&gsrprop=size|wordcount|timestamp|snippet
--- wiki_viewer/js/main.js
+++ wiki_viewer/js/main.js
@@ -5,27 +5,76 @@
 //&gsrinfo=totalhits|suggestion|rewrittenquery
 //&gsrprop=size|wordcount|timestamp|snippet
 
-var baseUrl = "https://en.wikipedia.org/w/api.php";
+var baseApiUrl = "https://en.wikipedia.org/w/api.php";
 var searchTerm = "flensburg";
-var query = "?action=query&format=json&prop=pageimages|extracts" +
-            "&generator=search&pithumbsize=200&exsentences=1" +
+
+
+//https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages|extracts&generator=search&piprop=thumbnail&pithumbsize=200&exsentences=2&gsrsearch=flensburg&gsrnamespace=0&gsrinfo=totalhits|suggestion|rewrittenquery&gsrprop=size|wordcount|timestamp|snippet&exintro
+
+var query = "action=query&format=json&prop=pageimages|extracts" +
+            "&generator=search&piprop=thumbnail&pithumbsize=200&exsentences=2" +
             "&gsrsearch=" + searchTerm + "&gsrnamespace=0" +
             "&gsrinfo=totalhits|suggestion|rewrittenquery" +
-            "&gsrprop=size|wordcount|timestamp|snippet";
-var fullUrl = baseUrl + query;
+            "&gsrprop=size|wordcount|timestamp|snippet" +
+            "&exintro";
+var payload = '{ "action": "query", "format": "json", "prop": "pageimages|extracts", "generator": "search", "piprop": "thumbnail", "pithumbsize": "200", "exsentences": "2", "gsrsearch": "flensburg", "gsrnamespace": "0", "gsrinfo": "totalhits|suggestion|rewrittenquery", "gsrprop": "size|wordcount|timestamp|snippet" }';
+var fullUrl = baseApiUrl + query;
 
-$.getJSON(fullUrl, (wikiPages) => {
-  // var country = weather.sys.country;
-  // var city = weather.name;
-  // var tempInC = weather.main.temp;
-  // var tempInF = (tempInC * 9 / 5 + 32).toFixed(2);
-  // var iconUrl = weather.weather[0].icon;
-  // tempInCText = '<img class="weatherIcon" src="' + iconUrl + '" alt="' + weather.weather[0].main + '" /><b>' + tempInC + ' °C</b><br>' + city + ' (' + country + ')';
-  // tempInFText = '<img class="weatherIcon" src="' + iconUrl + '" alt="' + weather.weather[0].main + '" /><b>' + tempInF + ' °F</b><br>' + city + ' (' + country + ')';
+var cardTemplate = '<a class="card" href="https://en.wikipedia.org/wiki/%pageTitle%">' + 
+'<span class="card-header" style="background-image: url(%pageImage%);">' + 
+'<span class="card-title"><h3>%pageTitle%</h3></span></span>' +
+'<span class="card-summary">%pageSummary%</span>' +
+'<span class="card-meta">%pageMeta%</span></a>';
 
-  // $("#details").html(tempInCText);     
-  // marker = L.marker([lat, lon])
-  //   .addTo(map)
-  //   .bindPopup(tempInCText)
-  //   .openPopup();
+function fetchData() {
+
+  // Using jQuery
+  $.ajax( {
+    url: baseApiUrl,
+    data: query,
+    dataType: 'jsonp',
+    type: 'POST',
+    jsonp: "callback",
+    success: function(data) {
+      var wikiPages = data.query.pages;
+      
+        var fullHtml = "";
+        for (var page in wikiPages) {
+          if (wikiPages.hasOwnProperty(page)) {
+            var card = cardTemplate.replace(/%pageTitle%/g, wikiPages[page].title);
+            if (wikiPages[page].hasOwnProperty("thumbnail"))
+            {
+              card = card.replace("%pageImage%", wikiPages[page].thumbnail.source);
+            }
+            else
+            {
+              card = card.replace("%pageImage%", "img/not-found.png");
+            }
+            card = card.replace("%pageSummary%", wikiPages[page].extract);
+            card = card.replace("%pageMeta%", "Some meta data.");
+            fullHtml += card;
+          }
+      }
+      $('#cards-container').html(fullHtml);
+    }
+  } );
+
+  // $.getJSON(fullUrl, (result) => {
+  //   var wikiPages = result.query.pages;
+  
+  //   var fullHtml = "";
+  //   for (var i = 0; i < wikiPages.length; i++)
+  //   {
+  //     var card = cardTemplate.replace("%pageTitle%", wikiPages[i].title);
+  //     card = card.replace("%pageImage%", wikiPages[i].thumbnail.source);
+  //     card = card.replace("%pageSummary%", wikiPages[i].extract);
+  //     card = card.replace("%pageMeta%", "Some meta data.");
+  //     fullHtml += card;
+  //   }
+  //   $('#cards-container').html(fullHtml);
+  // });
+}
+
+$(document).ready(function () {
+  fetchData();
 });
