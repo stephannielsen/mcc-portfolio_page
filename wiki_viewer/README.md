5d18012d8d9554b0cd5b91ab186c2e5fc0820022
Stephan Nielsen <stephannielsen@gmail.com>
2018-01-14 12:17:07 +0100

    Implemented basic card styles from codepen, hooked up the api request and building the UI dynamically.

--- wiki_viewer/README.md
+++ wiki_viewer/README.md
@@ -5,3 +5,5 @@ To run this webpage:
 - Install NodeJS
 - Run `npm install` in this folder
 - Run `npm start` opens index.html in FF/Chrome
+
+https://codepen.io/andytran/pen/BNjymy?q=cards&limit=all&type=type-pens
--- wiki_viewer/js/main.js
+++ wiki_viewer/js/main.js
@@ -6,25 +6,20 @@
 //&gsrprop=size|wordcount|timestamp|snippet
 
 var baseApiUrl = "https://en.wikipedia.org/w/api.php";
+var linkUrl = "https://wikipedia.org/wiki/";
 var searchTerm = "flensburg";
 
 
 //https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages|extracts&generator=search&piprop=thumbnail&pithumbsize=200&exsentences=2&gsrsearch=flensburg&gsrnamespace=0&gsrinfo=totalhits|suggestion|rewrittenquery&gsrprop=size|wordcount|timestamp|snippet&exintro
 
 var query = "action=query&format=json&prop=pageimages|extracts" +
-            "&generator=search&piprop=thumbnail&pithumbsize=200&exsentences=2" +
+            "&generator=search&piprop=thumbnail&pithumbsize=500&exsentences=2" +
             "&gsrsearch=" + searchTerm + "&gsrnamespace=0" +
             "&gsrinfo=totalhits|suggestion|rewrittenquery" +
             "&gsrprop=size|wordcount|timestamp|snippet" +
             "&exintro";
 var payload = '{ "action": "query", "format": "json", "prop": "pageimages|extracts", "generator": "search", "piprop": "thumbnail", "pithumbsize": "200", "exsentences": "2", "gsrsearch": "flensburg", "gsrnamespace": "0", "gsrinfo": "totalhits|suggestion|rewrittenquery", "gsrprop": "size|wordcount|timestamp|snippet" }';
 var fullUrl = baseApiUrl + query;
-
-var cardTemplate = '<a class="card" href="https://en.wikipedia.org/wiki/%pageTitle%">' + 
-'<span class="card-header" style="background-image: url(%pageImage%);">' + 
-'<span class="card-title"><h3>%pageTitle%</h3></span></span>' +
-'<span class="card-summary">%pageSummary%</span>' +
-'<span class="card-meta">%pageMeta%</span></a>';
 
 function fetchData() {
 
@@ -36,50 +31,97 @@ function fetchData() {
     type: 'POST',
     jsonp: "callback",
     success: function(data) {
-      var wikiPages = data.query.pages;
-
-
+      var wikiPages = data.query.pages; 
       
-        var fullHtml = "";
-        for (var page in wikiPages) {
-          if (wikiPages.hasOwnProperty(page)) {
-            var card = cardTemplate.replace(/%pageTitle%/g, wikiPages[page].title);
-            if (wikiPages[page].hasOwnProperty("thumbnail"))
-            {
-              card = card.replace("%pageImage%", wikiPages[page].thumbnail.source);
-            }
-            else
-            {
-              card = card.replace("%pageImage%", "img/not-found.png");
-            }
-            card = card.replace("%pageSummary%", wikiPages[page].extract);
-            card = card.replace("%pageMeta%", "Some meta data.");
-            fullHtml += card;
-          }
+      // setting up the columns
+      var numberOfColumns = 5;
+      var columns = [];
+      while (columns.length < numberOfColumns)
+        columns.push(document.createElement("div"));
+      
+      var columnIndex = 0;
+
+      for (var wikiPage in wikiPages) {
+        var postModule = document.createElement("div");
+        postModule.setAttribute("class", "post-module");
+        var thumbnail = document.createElement("div");
+        thumbnail.setAttribute("class", "thumbnail");
+        // var date = document.createElement("div");
+        // date.setAttribute("class", "date");
+        // var day = document.createElement("div");
+        // day.setAttribute("class", "day");
+        // var month = document.createElement("div");
+        // month.setAttribute("class", "month");
+        // date.appendChild(day);
+        // date.appendChild(month);
+        // thumbnail.appendChild(date);
+        var image = document.createElement("img");
+        if (wikiPages[wikiPage].hasOwnProperty("thumbnail")) {
+          image.setAttribute("src", wikiPages[wikiPage].thumbnail.source);
+        } else {
+          image.setAttribute("src", "img/not-found.png");
+        }
+        image.setAttribute("alt", wikiPages[wikiPage].title);
+        image.setAttribute("title", wikiPages[wikiPage].title);
+        thumbnail.appendChild(image);
+
+        postModule.appendChild(thumbnail);
+
+        var postContent = document.createElement("div");
+        postContent.setAttribute("class", "post-content");
+        // var category = document.createElement("div");
+        // category.setAttribute("class", "category");
+        // category.innerHTML = "Photos";
+        // postContent.appendChild(category);
+        var title = document.createElement("h1");
+        title.setAttribute("class", "title");
+        title.innerHTML = wikiPages[wikiPage].title;
+        postContent.appendChild(title);
+
+        var subTitle = document.createElement("h2");
+        subTitle.setAttribute("class", "sub_title");
+        postContent.appendChild(subTitle);
+
+        var description = document.createElement("p");
+        description.setAttribute("class", "description");
+        description.innerHTML = wikiPages[wikiPage].extract.replace("<p>", "").replace("</p>", "");
+        postContent.appendChild(description);
+
+        var postMeta = document.createElement("div");
+        postMeta.setAttribute("class", "post-meta");
+        var timestamp = document.createElement("span");
+        timestamp.setAttribute("class", "timestamp");
+        timestamp.innerHTML = '<a href="' + linkUrl + wikiPages[wikiPage].title + '" target="_blank">Read more...</a>';
+        var comments = document.createElement("span");
+        comments.setAttribute("class", "comments");
+        comments.innerHTML = '<i class="fa fa-comments"></i><a href="#"> 39 comments</a>';
+        postMeta.appendChild(timestamp);
+        postMeta.appendChild(comments);
+        postContent.appendChild(postMeta);
+
+        postModule.appendChild(postContent);
+
+        //add the postmodule to the correct column
+        columns[columnIndex].appendChild(postModule);
+        //calculate next column index and care for column break
+        columnIndex = (columnIndex + 1) % numberOfColumns;
       }
-      $('#cards-container').html(fullHtml);
+
+      // add columns to the container
+      columns.forEach(column => {
+        column.setAttribute("class", "column");
+        var container = document.getElementById('container');
+        container.appendChild(column);
+      });
     }
   } );
-
-  // $.getJSON(fullUrl, (result) => {
-  //   var wikiPages = result.query.pages;
-  
-  //   var fullHtml = "";
-  //   for (var i = 0; i < wikiPages.length; i++)
-  //   {
-  //     var card = cardTemplate.replace("%pageTitle%", wikiPages[i].title);
-  //     card = card.replace("%pageImage%", wikiPages[i].thumbnail.source);
-  //     card = card.replace("%pageSummary%", wikiPages[i].extract);
-  //     card = card.replace("%pageMeta%", "Some meta data.");
-  //     fullHtml += card;
-  //   }
-  //   $('#cards-container').html(fullHtml);
-  // });
 }
 
 $(document).ready(function () {
-  // fetchData();
-  
+  fetchData();
+});
+
+$(window).on("load", function() {
   $('.post-module').hover(function() {
     $(this).find('.description').stop().animate({
       height: "toggle",
@@ -87,12 +129,3 @@ $(document).ready(function () {
     }, 300);
   });
 });
-
-// $(window).load(function() {
-//   $('.post-module').hover(function() {
-//     $(this).find('.description').stop().animate({
-//       height: "toggle",
-//       opacity: "toggle"
-//     }, 300);
-//   });
-// });
